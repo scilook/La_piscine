bf2c1e95dba36203e4832ba0ac5b4ad2b5f1d767
Hyeongrok Son <hyeson@c1r12s2.42gyeongsan.kr>
2024-08-15 16:11:49 +0900

    files are turned in

--- ex08/ft_print_combn.c
+++ ex08/ft_print_combn.c
@@ -1,16 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_combn.c                                   :+:      :+:    :+:   */
+/*   ft_print_combn copy.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hyeson <hyeson@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/14 19:05:19 by hyeson            #+#    #+#             */
-/*   Updated: 2024/08/15 15:40:43 by hyeson           ###   ########.fr       */
+/*   Updated: 2024/08/15 15:54:42 by hyeson           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+
+void	ft_print_combn_lines(int *n, int *i, char *chnbr)
+{
+	*i = *n - 1;
+	while (chnbr[*i] < 0x3A)
+	{
+		write(1, chnbr, *n);
+		write(1, ", ", 2);
+		chnbr[*n - 1]++;
+	}
+}
 
 void	ft_print_combn(int n)
 {
@@ -36,13 +47,7 @@ void	ft_print_combn(int n)
 			chnbr[i + 1] = chnbr[i] + 1;
 			i ++;
 		}
-		i = n - 1;
-		while (chnbr[i] < 0x3A)
-		{
-			write(1, chnbr, n);
-			write(1, ", ", 2);
-			chnbr[n - 1]++;
-		}
+		ft_print_combn_lines(&n, &i, chnbr);
 	}
 	write(1, "\b\b", 2);
 }
