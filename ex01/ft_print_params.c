add901604ed12c94909a8a00b2c716b7243a5608
Hyeongrok Son <hyeson@c2r17s6.42gyeongsan.kr>
2024-08-25 12:45:34 +0900

    turn in

---
+++ ex01/ft_print_params.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyeson <hyeson@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/24 20:30:52 by hyeson            #+#    #+#             */
+/*   Updated: 2024/08/24 20:34:58 by hyeson           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_strlen(char *str)
+{
+	int	cnt;
+
+	cnt = 0;
+	while (*(str + cnt) != '\0')
+		cnt++;
+	return (cnt);
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	i = 1;
+	while (i < argc)
+	{
+		write(1, argv[i], ft_strlen(argv[i]));
+		write(1, "\n", 1);
+	}
+}
