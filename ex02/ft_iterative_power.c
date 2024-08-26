5ff687fce4a5addbdd9416a6ea46e1f3b9520cfe
Hyeongrok Son <hyeson@c1r8s6.42gyeongsan.kr>
2024-08-26 20:23:06 +0900

    turn in

--- ex02/ft_iterative_power.c
+++ ex02/ft_iterative_power.c
@@ -6,20 +6,22 @@
 /*   By: hyeson <hyeson@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/24 21:05:13 by hyeson            #+#    #+#             */
-/*   Updated: 2024/08/24 22:23:36 by hyeson           ###   ########.fr       */
+/*   Updated: 2024/08/26 20:22:12 by hyeson           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_iterative_power(int nb, int power)
 {
 	int	cnt;
+	int	value;
 
-	cnt = 1;
+	cnt = 0;
+	value = nb;
 	if (power < 0)
 		return (0);
 	if (power == 0)
 		return (1);
 	while (++cnt < power)
-		nb *= nb;
+		nb *= value;
 	return (nb);
 }
