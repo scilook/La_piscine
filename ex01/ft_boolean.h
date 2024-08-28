9ce3e5a5be5dbf483569f2514f5f2095b4cad797
Hyeongrok Son <hyeson@c1r22s8.42gyeongsan.kr>
2024-08-28 15:18:35 +0900

    turn in

---
+++ ex01/ft_boolean.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyeson <hyeson@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/26 12:02:26 by hyeson            #+#    #+#             */
+/*   Updated: 2024/08/28 15:09:55 by hyeson           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+
+typedef enum boolean
+{
+	false,
+	true
+}	t_bool;
+# define TRUE true
+# define FALSE false
+# define EVEN(nbr) (nbr%2==0)
+# define EVEN_MSG "I have an even number of arguments."
+# define ODD_MSG "I have an odd number of arguments."
+# define SUCCESS (0)
+
+#endif /* FT_BOOLEAN_H */
