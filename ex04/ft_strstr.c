a436575554b2a093aa91836cb46f73bcf9a8974a
Hyeongrok Son <hyeson@c1r22s8.42gyeongsan.kr>
2024-08-30 10:41:29 +0900

    v2 within strncmp

--- ex04/ft_strstr.c
+++ ex04/ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: hyeson <hyeson@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/21 09:30:53 by hyeson            #+#    #+#             */
-/*   Updated: 2024/08/25 20:24:19 by hyeson           ###   ########.fr       */
+/*   Updated: 2024/08/30 10:40:51 by hyeson           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,36 +20,26 @@ int	ft_strlen(char *str)
 	return (cnt);
 }
 
-int	ft_strncmp(char *s1, char *s2, unsigned int n)
-{
-	unsigned int	i;
-
-	i = -1;
-	if (ft_strlen(s1) > ft_strlen(s2))
-	{
-		while (s1[++i] != '\0' && i < n)
-			if (s1[i] != s2[i])
-				return (s1[i] - s2[i]);
-	}
-	else
-	{
-		while (s2[++i] != '\0' && i < n)
-			if (s1[i] != s2[i])
-				return (s1[i] - s2[i]);
-	}
-	return (0);
-}
-
 char	*ft_strstr(char *str, char *to_find)
 {
 	int	i;
+	int	j;
+	int result;
 
 	i = -1;
+	j = -1;
 	if (to_find[0] == '\0')
 		return (str);
 	while (str[++i] != '\0')
 		if (to_find[0] == str[i])
-			if (ft_strncmp(to_find, str + i, ft_strlen(to_find)) == 0)
+		{
+			while (to_find[++j] != '\0' || (str + i)[j] != '\0')
+				if (j < ft_strlen(to_find))
+					if (to_find[j] != (str + i)[j])
+						result = (to_find[j] - (str + i)[j]);
+			result = (0);
+			if (result == 0)
 				return (str + i);
-	return ("\0");
+		}
+	return (0);
 }
